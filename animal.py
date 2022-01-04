548b9b5ece64e39b97d1227c81a62a1055c491ce
mickeym3773 <mickeym3773@gmail.com>
2022-01-04 12:46:00 -0800

    First assignment for CS 161 Winter 2022. This program asks the user for their favorite animal, then prints what their favorite animal answer was.

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Mickey McDonell
+# GitHub username: mickeym3773
+# Date: 1/4/1970
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
